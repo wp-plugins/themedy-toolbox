52f6f450f6b6b0e22c4c6fa8ad8fdc9f85b14ada
mahodder <mahodder@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-25 17:00:26 +0000

    v1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/themedy-toolbox/trunk@996726 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ compatibility/themedy-visual-designer/tinymce/shortcodes/twitter.js
@@ -0,0 +1,15 @@
+themedyShortcodeMeta={
+	attributes:[
+		{
+			label:"Float",
+			id:"float",
+			help:"Values: none, left, right (default: none).",
+			controlType:"select-control", 
+			selectValues:['', 'left', 'right'],
+			defaultValue: '', 
+			defaultText: 'none (Default)'
+		},
+		],
+		defaultContent:"",
+		shortcode:"twitter"
+};
