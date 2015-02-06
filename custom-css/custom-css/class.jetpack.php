50c9035abdaf52f325b03f719727c83e33f7f392
mahodder <mahodder@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-06 15:31:37 +0000

    1.0.4
    git-svn-id: https://plugins.svn.wordpress.org/themedy-toolbox/tags/1.0.4@1083880 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ custom-css/custom-css/class.jetpack.php
@@ -0,0 +1,11 @@
+<?php
+
+class Jetpack {
+	/**
+	 * Get $content_width, but with a <s>twist</s> filter.
+	 */
+	public static function get_content_width() {
+		$content_width = isset( $GLOBALS['content_width'] ) ? $GLOBALS['content_width'] : false;
+		return apply_filters( 'jetpack_content_width', $content_width );
+	}
+}
